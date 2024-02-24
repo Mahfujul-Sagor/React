dea2f6b6767fd431a7e5ce596ded5a74aeae37b4
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-02-24 10:29:33 +0600

    second-file add

---
+++ second-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='heading'>hello world!</h1>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App
