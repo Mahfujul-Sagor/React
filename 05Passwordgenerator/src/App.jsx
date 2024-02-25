85d1d568b6194937141ed66f8a6af0222ba39de9
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-02-25 19:14:50 +0600

    done

--- 05Passwordgenerator/src/App.jsx
+++ 05Passwordgenerator/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -14,20 +16,29 @@ function App() {
     if (charAllowed) str += '@!%#$^&*()*_';
 
     for (let i = 0; i < length; i++) {
-      const value = str.charAt(Math.floor(Math.random() * str.length));
-      pass += value;
+      const char = str.charAt(Math.floor(Math.random() * str.length));
+      pass += char;
     }
     setPassword(pass);
+
   }, [length, numberAllowed, charAllowed]);
 
   useEffect(() => {
     generatePassword();
   }, [length, numberAllowed, charAllowed]);
 
+  const copyPassword = ()=> {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+
+  }
+
+  const passwordRef = useRef(null);
+
   return (
-    <div className="min-h-screen bg-zinc-800 text-white flex justify-center items-center">
+    <div className="min-h-screen bg-zinc-800 font-semibold flex justify-center items-center">
       <div className="bg-teal-700 w-full max-w-md shadow-md rounded-lg px-4 py-3 my-8">
-        <h1 className="text-3xl font-semibold my-3 text-center">
+        <h1 className="text-3xl text-white font-semibold my-3 text-center">
           Password Generator
         </h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
@@ -35,17 +46,20 @@ function App() {
             className="w-full px-3 py-1 outline-none"
             type="text"
             value={password}
-            placeholder='password'
+            placeholder="password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="bg-blue-400 px-3 py-1">Copy</button>
+          <button onClick={copyPassword} className="bg-blue-400 px-4 py-2">
+            <FontAwesomeIcon icon={faCopy} />
+          </button>
         </div>
         <div className="flex text-sm gap-x-4">
           <div className="flex items-center gap-x-1 p-1">
             <input
               type="range"
               min={8}
-              max={100}
+              max={30}
               name=""
               id=""
               value={length}
