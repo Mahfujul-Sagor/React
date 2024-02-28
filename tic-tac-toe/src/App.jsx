3719812b312cba4e6c51e42bb6f3f3938bbe451f
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-02-28 19:50:55 +0600

    add new project

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full min-h-screen bg-gray-600'>
+      
+    </div>
+  )
+}
+
+export default App;
