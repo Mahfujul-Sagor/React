b68921bbeaae3b2aaa03d9fc4601221da36dc3a7
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-03-17 14:08:59 +0600

    updater function

--- brocodeprops/src/components/Counter.jsx
+++ brocodeprops/src/components/Counter.jsx
@@ -5,12 +5,12 @@ function Counter() {
     const [count, setCount] = useState(0);
 
     const increment = ()=> {
-        setCount(count + 1 );
+        setCount(c => c + 1);
     };
 
     const decrement = ()=> {
         if (count > 0) {
-            setCount(count - 1);
+            setCount(c => c - 1);
         }else {
             return;
         }
